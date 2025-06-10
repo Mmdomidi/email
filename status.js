eecf030672e0e99c241a2e770570fd8610960a90
Mmdomidi <mamygy1381@gmail.com>
2025-06-10 08:00:31 +0330

    Update status.js

--- status.js
+++ status.js
@@ -49,7 +49,7 @@ async function reserveEmail() {
   try {
     const formData = new URLSearchParams({ name });
 
-    const response = await fetch("https://aland-learning.darkube.app/webhook-test/reserve", {
+    const response = await fetch("https://aland-learning.darkube.app/webhook/reserve", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
